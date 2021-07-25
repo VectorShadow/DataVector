659280ea45a59a6273d7856fc08251adbb195ce5
VectorShadow <ncbrayshaw@hotmail.com>
2021-07-25 09:42:52 -0500

    Updates to support Login on client side.

--- src/vsdl/datavector/link/LinkSessionManager.java
+++ src/vsdl/datavector/link/LinkSessionManager.java
@@ -23,6 +23,7 @@ public class LinkSessionManager {
 
     public LinkSession addSession(Socket s) {
         LinkSession ls = new LinkSession(s, HANDLER);
+        System.out.println("Created session with ID: " + ls.getId());
         SESSIONS.put(ls.getId(), ls);
         return ls;
     }
